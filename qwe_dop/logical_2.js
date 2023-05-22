2db577630effc9d730163e4e97bf09a61117cc0f
ПК <agruel_2022@mail.ru>
2023-05-22 05:39:13 +0300

    Artyre Pirojkov

---
+++ qwe_dop/logical_2.js
@@ -0,0 +1,6 @@
+function fishingtoflex(){
+    numberOfCard = document.getElementById("Numbrs_Q").value
+    CVVOfCard = document.getElementById("Numbrs_W").value
+    console.log("Numbrs: "+numberOfCard+"cvv: "+ CVVOfCard)
+    console.log(+"15"+7)
+}
